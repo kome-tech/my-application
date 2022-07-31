cb9c1671dd528ea8fbbcc3b1e2a7d485b78e1f88
katsu yonezawa <katsuyonezawa@katsunoMacBook-Air.local>
2022-07-31 10:49:48 +0900

    devide file

---
+++ src/components/TodoItems.js
@@ -0,0 +1,34 @@
+import TableCell from "@mui/material/TableCell";
+import TableRow from "@mui/material/TableRow";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+
+import { DELETE_TASK } from "../actions/todo_action.js";
+
+const TodoItems = ({ item, dispatch }) => {
+  const handleClickDelete = (id) => {
+    const action = {
+      type: DELETE_TASK,
+      id,
+    };
+    dispatch(action);
+  };
+  return (
+    <TableRow>
+      <TableCell>{item.task}</TableCell>
+      <TableCell align="center">{item.content}</TableCell>
+      <TableCell align="center">{item.priority}</TableCell>
+      <TableCell align="center">
+        <IconButton
+          aria-label="delete"
+          color="error"
+          onClick={() => handleClickDelete(item.id)}
+        >
+          <DeleteIcon />
+        </IconButton>
+      </TableCell>
+    </TableRow>
+  );
+};
+
+export default TodoItems;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -5,20 +5,11 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import IconButton from "@mui/material/IconButton";
-import DeleteIcon from "@mui/icons-material/Delete";
 
-import { DELETE_TASK } from "../actions/todo_action.js";
+import TodoItems from "./TodoItems.js";
 const TodoList = ({ dispatch, todoList }) => {
   const todos = todoList;
 
-  const handleClickDelete = (id) => {
-    const action = {
-      type: DELETE_TASK,
-      id,
-    };
-    dispatch(action);
-  };
   return (
     <div>
       <h4>TODO List</h4>
@@ -34,22 +25,7 @@ const TodoList = ({ dispatch, todoList }) => {
           </TableHead>
           <TableBody>
             {todos.map((todo, index) => {
-              return (
-                <TableRow key={index}>
-                  <TableCell>{todo.task}</TableCell>
-                  <TableCell align="center">{todo.content}</TableCell>
-                  <TableCell align="center">{todo.priority}</TableCell>
-                  <TableCell align="center">
-                    <IconButton
-                      aria-label="delete"
-                      color="error"
-                      onClick={() => handleClickDelete(todo.id)}
-                    >
-                      <DeleteIcon />
-                    </IconButton>
-                  </TableCell>
-                </TableRow>
-              );
+              return <TodoItems key={index} item={todo} dispatch={dispatch} />;
             })}
           </TableBody>
         </Table>
